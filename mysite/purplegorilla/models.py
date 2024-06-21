c392ca433864ee30cde56c5f09840101be79f7ea
Debiddo <dzheng@terpmail.umd.edu>
2024-06-21 12:41:01 -0400

    created a product model and hooked up psql

---
+++ mysite/purplegorilla/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+CATEGORIES = (
+    ('G', 'Generic'),
+    ('E', 'Electronics'),
+    ('F', 'Fashion'),
+    ('H&F', 'Home and Furnitures'),
+    ('B&H', 'Beauty and Health'),
+    ('B&M', 'Books and Media'),
+    ('S&O', 'Sports and Outdoor'),
+    ('T&B', 'Toys and Baby Products'),
+    ('F&B', 'Food and Beverages')
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    price1 = models.FloatField()
+    description = models.TextField(max_length=250)
+    category = models.CharField(
+        max_length=3,
+        choices=CATEGORIES,
+        default=CATEGORIES[0][0]
+    )
+    image = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
