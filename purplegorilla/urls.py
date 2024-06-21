cd0d8d155cd489646c6ac8407f59dc10a85202de
Debiddo <dzheng@terpmail.umd.edu>
2024-06-21 16:58:55 -0400

    created a base.html with navbar

--- purplegorilla/urls.py
+++ purplegorilla/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views  # Import views to connect routes to view functions
 
 urlpatterns = [
-    path('',views.index,name='index'),
+    path('', views.index, name='index'),
+    path('base', views.base, name='base'),
 ]
--- purplegorilla/views.py
+++ purplegorilla/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .models import Product
 # Create your views here.
 
+
 def index(request):
     products = Product.objects.all()
-    return render(request,'purplegorilla/index.html',{'products':products})
+    return render(request, 'purplegorilla/index.html', {'products': products})
+
+
+def base(request):
+    return render(request, 'base.html')
