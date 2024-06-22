83d9a8f4470b550c8790f7018ebdc96dc1892f83
Debiddo <dzheng@terpmail.umd.edu>
2024-06-22 15:22:40 -0400

    added search functionality

--- purplegorilla/views.py
+++ purplegorilla/views.py
@@ -9,6 +9,9 @@ from .models import Product
 
 def index(request):
     products = Product.objects.all()
+    product_name = request.GET.get('product_name')
+    if product_name != '' and product_name is not None:
+        products = Product.objects.filter(name__icontains=product_name)
     return render(request, 'purplegorilla/index.html', {'products': products})
 
 
