f8e3b68db545d5831e23db83d83304afa75f46ff
Debiddo <dzheng@terpmail.umd.edu>
2024-06-28 00:02:29 -0400

    created a read me

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,6 +144,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = 'product-index'
+LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'home'
